f1960f830e99dd4e18e1123c3eed0647004b06e5
Allan Reis <47274495+allanpreis@users.noreply.github.com>
2021-04-23 23:12:58 -0300

    Add files via upload

---
+++ Loja/src/CalculadoraDeImposto.java
@@ -0,0 +1,16 @@
+import java.math.BigDecimal;
+
+public class CalculadoraDeImposto {
+
+    public BigDecimal calcular(Pedido pedido){
+        /*
+         * ICMS 10%
+         * IPI 5%
+         * ISS 2%
+         */
+
+
+
+
+    }
+}
